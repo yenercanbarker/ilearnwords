33283feb795a51d529010eca564f953a8821efd9
Yenercan Barker <yenercanbarker@gmail.com>
2019-05-10 15:13:11 +0300

    Delete veritabani.php

--- ödev vol5/veritabani.php
+++
@@ -1,9 +0,0 @@
-<?php
-$username="root";  
-$password="";  
-$hostname = "localhost";  
-$db = "kelimelerproje";
-
-$baglan = new PDO("mysql:hostname=$hostname;db=$db;charset=utf8;",$username,$password);
-
-?>
